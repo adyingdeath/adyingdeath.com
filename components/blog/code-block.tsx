de882f27f7f85245b25d84b68ca78bc1a7dcd496
adyingdeath <2975527435@qq.com>
2026-06-01 20:34:01 +0800

    feat: add filename and lang display to code block top bar.

--- components/blog/code-block.tsx
+++ components/blog/code-block.tsx
@@ -4,27 +4,41 @@ import { highlight } from "@/lib/highlight/highligher";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { mapleMono } from "./font";
 
-export async function CodeBlock({ code = "", language = "plaintext" }: {
+export async function CodeBlock({
+  code = "",
+  language = "plaintext",
+  filename,
+}: {
   code: string,
   language: string,
+  filename?: string,
 }) {
   const out = await highlight(code, language);
 
   return (
     <div className="flex">
-      <ScrollArea className="w-1 flex-1 rounded-sm border">
-        <div className={cn(mapleMono.className, "flex")}>
-          <div
-            className={cn(
-              "**:font-[inherit]", // Use mapleMono font
-              "[&>pre]:p-4 flex-1",
-              "text-xs"
-            )}
-            dangerouslySetInnerHTML={{ __html: out }}
-          />
+      <div className="w-1 flex-1 rounded-sm border overflow-clip bg-muted">
+        {/* Top bar — outside scroll area */}
+        <div className="flex items-center justify-between px-4 py-1.5 text-xs text-muted-foreground">
+          <span>{filename}</span>
+          <span>{language}</span>
         </div>
-        <ScrollBar orientation="horizontal" className="m-0.5" />
-      </ScrollArea>
+        <div className="px-1 pb-1">
+          <ScrollArea className="w-full rounded-sm">
+            <div className={cn(mapleMono.className, "flex")}>
+              <div
+                className={cn(
+                  "**:font-[inherit]",
+                  "[&>pre]:p-4 flex-1",
+                  "text-xs",
+                )}
+                dangerouslySetInnerHTML={{ __html: out }}
+              />
+            </div>
+            <ScrollBar orientation="horizontal" className="m-0.5" />
+          </ScrollArea>
+        </div>
+      </div>
     </div>
   );
 }
